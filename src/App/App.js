2c5ade7269a51822304a6739ab6ae46c0a874fac
Lochbird <130000036+Lochbird@users.noreply.github.com>
2024-04-14 18:26:04 -0700

    More Fixes On File Structure & Reinstall Lost Assets
    Fixing the file structure caused me to lose the original package.json files rather than the duplicates. /Components was located in the /App as well and I made sure to reroute the previously incorrect file paths

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { TodoWrapper } from "./Components/TodoWrapper/TodoWrapper";
+import { TodoWrapper } from "../Components/TodoWrapper/TodoWrapper";
 
 function App() {
   return (
     <div className="App">
       <TodoWrapper />
+      word
     </div>
   );
 }
